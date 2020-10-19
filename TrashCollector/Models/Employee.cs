8267615c4ab7e1ec7c944e09dab4fa180f427afa
Josh Grimaldi <josh.grimaldi@outlook.com>
2020-10-19 16:23:42 -0400

    Added Customer and Employee Models, added all now known variables for the customer. Next I will be creating all known variables for Employee model.

---
+++ TrashCollector/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Models
+{
+    public class Employee
+    {
+        [Key]
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string PickupDayChoice { get; set; }
+        public string SuspendPickup { get; set; }
+        public double Balance { get; set; }
+
+    }
+}
