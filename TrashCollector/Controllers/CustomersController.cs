4c63ad59228067de8092c3f01882279b6685f365
Josh Grimaldi <josh.grimaldi@outlook.com>
2020-10-20 10:06:43 -0400

    Added following logic to cutomerController: Delete, edit, create, and index

--- TrashCollector/Controllers/CustomersController.cs
+++ TrashCollector/Controllers/CustomersController.cs
@@ -4,39 +4,53 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TrashCollector.Data;
+using TrashCollector.Models;
 
 namespace TrashCollector.Controllers
 {
     public class CustomersController : Controller
     {
+        private readonly ApplicationDbContext db;
+
+        public CustomersController(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+
         // GET: CustomersController
         public ActionResult Index()
         {
-            return View();
+            var customersInDatabase = db.Customers;
+            return View(customersInDatabase);
         }
 
         // GET: CustomersController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var customerToView = db.Customers.Where(c => c.Id == id).SingleOrDefault();
+            return View(customerToView);
         }
 
         // GET: CustomersController/Create
         public ActionResult Create()
         {
-            return View();
+            Customer customer = new Customer();
+            return View(customer);
         }
 
         // POST: CustomersController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Customer customer)
         {
             try
             {
+                db.Customers.Add(customer);
+                db.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch(Exception e)
             {
                 return View();
             }
@@ -45,19 +59,22 @@ namespace TrashCollector.Controllers
         // GET: CustomersController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var customerToEdit = db.Customers.Where(c => c.Id == id).SingleOrDefault();
+            return View(customerToEdit);
         }
 
         // POST: CustomersController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, Customer customer)
         {
             try
             {
+                db.Customers.Update(customer);
+                db.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch(Exception e)
             {
                 return View();
             }
@@ -66,19 +83,23 @@ namespace TrashCollector.Controllers
         // GET: CustomersController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
-        }
+            var customertoDelete = db.Customers.Where(c => c.Id == id).SingleOrDefault();
+            return View(customertoDelete);
+
+        } 
 
         // POST: CustomersController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(Customer customer)
         {
             try
             {
+                db.Customers.Remove(customer);
+                db.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch(Exception e)
             {
                 return View();
             }
