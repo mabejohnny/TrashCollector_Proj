b96ce0ada1c085fb160a229629fb3d8da86339ad
Josh Grimaldi <josh.grimaldi@outlook.com>
2020-10-20 09:01:20 -0400

    Create Id variable for both employee and customer

--- TrashCollector/Models/Customer.cs
+++ TrashCollector/Models/Customer.cs
@@ -9,6 +9,7 @@ namespace TrashCollector.Models
     public class Customer
     {
         [Key]
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
--- TrashCollector/Models/Employee.cs
+++ TrashCollector/Models/Employee.cs
@@ -9,7 +9,7 @@ namespace TrashCollector.Models
     public class Employee
     {
         [Key]
-
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string ZipCode { get; set; }
